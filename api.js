4214287070f7df0c36db8b91d45d99463542384e
GyeongSuKim <rla04722@naver.com>
2022-05-17 11:37:07 +0900

    Feat : Login 창과 연동

--- api.js
+++ api.js
@@ -2,16 +2,21 @@
 // 비 동기적으로 작성 예정
 async function handlesignin() {
 
-    const signupData = {   
-        email : document.getElementById('floatingInput').value,
-        password : document.getElementById('floatingPassword').value
+    const signupData = {
+        email: document.getElementById('floatingInput').value,
+        password: document.getElementById('floatingPassword').value
     }
 
     const response = await fetch('http://127.0.0.1:5000/signup', {
         method: 'POST',
         body: JSON.stringify(signupData)
-    }) 
+    })
 
-    console.log(response)
-
+    if (response.status == 200) {
+        alert('환영합니다~')
+        window.location.replace('http://127.0.0.1:5500/turtlegram_frontend/login.html')
+    } else {
+        alert('로그인 해라')
+        window.location.reload()
+    }
 }
