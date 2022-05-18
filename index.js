cb38122c767eaa6de763bf73d6698e68159abb98
GyeongSuKim <rla04722@naver.com>
2022-05-18 23:55:54 +0900

    Feat : 메인 UI, API

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-console.log('여기는 index.js')
+async function loadArticles() {
+    console.log('Here')
+    articles = await getArticles()
 
+    const article_list = document.getElementById("articles");
+    
+    articles.forEach(article => {
+        console.log(article)
+        const newArticle = document.createElement("li");
+        newArticle.innerText = article.title
+        article_list.appendChild(newArticle)
+    });
+
+
+
+}
+
+
+loadArticles();
 getName();
